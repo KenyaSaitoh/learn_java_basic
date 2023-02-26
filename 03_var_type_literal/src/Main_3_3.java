de9fe95b8dc04d32289072f9bd69b48c97c1d025
KenyaSaitoh <kenya.saitoh@gmail.com>
2023-02-26 09:46:58 +0900

    軽微な修正

--- 03_var_type_literal/src/Main_3_3.java
+++ 03_var_type_literal/src/Main_3_3.java
@@ -1,91 +1,91 @@
 public class Main_3_3 {
     public static void main(String[] args) {
         {
-            System.out.println("***** snippet_3_4_1 *****");
+            System.out.println("***** snippet_3_3_1 *****");
             int x = 10;
             int y = x; // 変数yに10が代入される
         }
         {
-            System.out.println("***** snippet_3_4_2 *****");
+            System.out.println("***** snippet_3_3_2 *****");
             String str1 = "foo";
             String str2 = str1; // 変数str2の文字列は"foo"
         }
         {
-            System.out.println("***** snippet_3_4_3 *****");
+            System.out.println("***** snippet_3_3_3 *****");
             int x = 10;
             int y = x; // 変数yに10が代入される
             x = 20;
             System.out.println(y); // どうなる？
         }
         {
-            System.out.println("***** snippet_3_4_4 *****");
+            System.out.println("***** snippet_3_3_4 *****");
             String str1 = "foo";
             String str2 = str1; // 変数str2の文字列は"foo"
             str1 = "bar";
             System.out.println(str2); // どうなる？
         }
         {
-            System.out.println("***** snippet_3_4_5 *****");
+            System.out.println("***** snippet_3_3_5 *****");
             //int x = "abc"; // 文字列をint型に代入
             //String str = 10; // 数値をString型に代入
         }
         {
-            System.out.println("***** snippet_3_4_6 *****");
+            System.out.println("***** snippet_3_3_6 *****");
             int x = 100;
             long y = x;
         }
         {
-            System.out.println("***** snippet_3_4_7 *****");
+            System.out.println("***** snippet_3_3_7 *****");
             float f = 12345.67F;
             double d = f;
         }
         {
-            System.out.println("***** snippet_3_4_8 *****");
+            System.out.println("***** snippet_3_3_8 *****");
             int x = 100;
             // short y = x; // コンパイルエラー
         }
         {
-            System.out.println("***** snippet_3_4_9 *****");
+            System.out.println("***** snippet_3_3_9 *****");
             int x = 100;
             short y = (short) x; // コンパイルエラーは解消
         }
         {
-            System.out.println("***** snippet_3_4_10 *****");
+            System.out.println("***** snippet_3_3_10 *****");
             int x = 400;
             byte y = (byte) x;
         }
         {
-            System.out.println("***** snippet_3_4_11 *****");
+            System.out.println("***** snippet_3_3_11 *****");
             double d = 12345.67;
             int x = (int) d;
         }
         {
-            System.out.println("***** snippet_3_4_12 *****");
+            System.out.println("***** snippet_3_3_12 *****");
             long x = 100;
         }
         {
-            System.out.println("***** snippet_3_4_13 *****");
-            //long x = 3_000_000_000; // 30億はint型の範囲外
+            System.out.println("***** snippet_3_3_13 *****");
+            // long x = 3_000_000_000; // リテラル30億はint型の範囲外
         }
         {
-            System.out.println("***** snippet_3_4_14 *****");
+            System.out.println("***** snippet_3_3_14 *****");
             long x = 3_000_000_000L;
         }
         {
-            System.out.println("***** snippet_3_4_15 *****");
+            System.out.println("***** snippet_3_3_15 *****");
             float f = 12345.67F;
         }
         {
-            System.out.println("***** snippet_3_4_16 *****");
+            System.out.println("***** snippet_3_3_16 *****");
             var x = 10; // int型と推論される
         }
         {
-            System.out.println("***** snippet_3_4_17 *****");
+            System.out.println("***** snippet_3_3_17 *****");
             var d = 12345.67; // double型と推論される
             var str = "foo"; // String型と推論される
         }
         {
-            System.out.println("***** snippet_3_4_18 *****");
+            System.out.println("***** snippet_3_3_18 *****");
             //var x; // コンパイルエラー
             //var x = 10, y = 20; // コンパイルエラー
         }
