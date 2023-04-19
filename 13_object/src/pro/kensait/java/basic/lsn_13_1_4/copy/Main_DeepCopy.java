f6de63f6dcf66370c728048691c4aa729cf7f39f
KenyaSaitoh <kenya.saitoh@gmail.com>
2023-04-19 12:52:12 +0900

    軽微な修正

--- 13_object/src/pro/kensait/java/basic/lsn_13_1_4/copy/Main_DeepCopy.java
+++ 13_object/src/pro/kensait/java/basic/lsn_13_1_4/copy/Main_DeepCopy.java
@@ -3,12 +3,12 @@ package pro.kensait.java.basic.lsn_13_1_4.copy;
 public class Main_DeepCopy {
     public static void main(String[] args) {
         Address a1 = new Address("103-0004", "中央区", "1-1-1");
-        Person p1 = new Person("Alice", 25, a1);
+        Person person = new Person("Alice", 25, a1);
         Address a2 = new Address(a1.getZipCode(),
                 a1.getCity(),
                 a1.getAddressLine());
-        Person p2 = new Person(p1.getName(), p1.getAge(), a2);
-        p1.getAddress().setAddressLine("2-2-2");
-        System.out.println(p2.getAddress());
+        Person copy = new Person(person.getName(), person.getAge(), a2);
+        person.getAddress().setAddressLine("2-2-2");
+        System.out.println(copy.getAddress());
     }
 }
