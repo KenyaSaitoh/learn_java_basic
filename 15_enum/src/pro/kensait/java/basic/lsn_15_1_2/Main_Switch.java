50e3be4524521664fcd659c7eb5297f224e80e10
Kenya Saitoh <kenya.saitoh@gmail.com>
2024-10-27 19:00:46 +0900

    cc

--- 15_enum/src/pro/kensait/java/basic/lsn_15_1_2/Main_Switch.java
+++ 15_enum/src/pro/kensait/java/basic/lsn_15_1_2/Main_Switch.java
@@ -3,17 +3,10 @@ package pro.kensait.java.basic.lsn_15_1_2;
 public class Main_Switch {
     public static void main(String[] args) {
         Customer customer = new Customer(1, "Alice", CustomerType.PLATINUM);
-        int deliveryFee;
-        switch (customer.getCustomerType()) { // int型変数
-        case GENERAL, GOLD: // 一般会員またはゴールド会員を表すラベル
-            deliveryFee = 900;
-            break;
-        case PLATINUM: // プラチナ会員を表すラベル
-            deliveryFee = 600;
-            break;
-        case DIAMOND: // ダイヤモンド会員を表すラベル
-            deliveryFee = 0;
-            break;
-        }
+        int deliveryFee = switch(customer.getCustomerType()) {
+        case GENERAL, GOLD -> 900; // 一般会員またはゴールド会員を表すラベル
+        case PLATINUM -> 600; // プラチナ会員を表すラベル
+        case DIAMOND -> 0; // ダイヤモンド会員を表すラベル
+        };
     }
 }
